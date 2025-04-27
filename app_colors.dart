a5ebed43fc68da1c2e54a6b6dacb2c5aed530f4f
jgoleta <jgoleta@gbox.adnu.edu.ph>
2025-04-27 22:14:45 +0800

    Create app_colors.dart

---
+++ app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color headingText = Color(0xFF1F214D);
+  static const Color subheadingText = Color(0xFFA0A5BA);
+  static const Color primaryButton = Color(0xFFFFBD69);
+  static const Color indicatorDot = Color(0xFFD9D9D9);
+}
